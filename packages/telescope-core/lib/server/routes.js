b649f048b763ad620845ef94219b660862dab10c
Bharat JIlledumudI <bharat.jilledumudi@gmail.com>
2015-10-24 22:15:45 +0530

    Still trying to fix the issue with redirection of product URLs on Heroku

--- packages/telescope-core/lib/server/routes.js
+++ packages/telescope-core/lib/server/routes.js
@@ -20,12 +20,17 @@ var increasePostClicks = function(postId, ip){
 Picker.route('/out', function(params, req, res, next) {
   var query = params.query;
   if(query.url){ // for some reason, query.url doesn't need to be decoded
+      console.log("Step-1");
     var post = Posts.findOne({url: query.url});
     if (post) {
+      console.log("Step-2");
       var ip = req.connection.remoteAddress;
       increasePostClicks(post._id, ip);
-      res.writeHead(302, {'Location': query.url, 'content-type': 'text/html', 'X-Frame-Options': 'deny'});
+      console.log("Step-3");
+      res.writeHead(302, {'Location': query.url});
+      console.log("Step-4");
       res.end();
+      console.log("Step-5");
     } else {
       // don't redirect if we can't find a post for that link
       res.end('Invalid URL');
